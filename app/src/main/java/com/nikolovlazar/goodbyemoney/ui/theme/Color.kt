6460c9c4b1130c0bd05ec0d83b6fdc92b76568e9
Shreya-Ve <101716641+Shreya-Ve@users.noreply.github.com>
2023-10-29 20:32:55 +0530

    Update Color.kt

--- app/src/main/java/com/nikolovlazar/goodbyemoney/ui/theme/Color.kt
+++ app/src/main/java/com/nikolovlazar/goodbyemoney/ui/theme/Color.kt
@@ -2,14 +2,14 @@ package com.nikolovlazar.goodbyemoney.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Surface = Color.Black
+val Surface = Color(0xFF423E3A)
 val TextPrimary = Color.White
 val Destructive = Color(0xFFFF453A)
-val Primary = Color(0xFF0A84FF)
+val Primary = Color(0xFF64ACC4)
 
 val SystemGray04 = Color(0xFF3A3A3C)
-val BackgroundElevated = Color(0xFF1C1C1E)
-val LabelSecondary = Color(0xFF8E8E93)
-val DividerColor = Color(0x20787880)
-val TopAppBarBackground = Color(0x5E1D1D1D)
+val BackgroundElevated = Color(0xFF6D6760)
+val LabelSecondary = Color(0xFFF0DFC8)
+val DividerColor = Color(0xFF7C898B)
+val TopAppBarBackground = Color(0xFFB29E84)
 val FillTertiary = Color(0x18767680)
